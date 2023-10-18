a6cf604e160c06d67b1b1892d16d4349a447f053
Silu Hu <erinh816@gmail.com>
2023-10-17 20:17:25 -0400

    finish 2 basic tests

--- markov.test.js
+++ markov.test.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const { MarkovMachine } = require("./markov.js");
+
+let text, markovMachine, chains;
+
+beforeEach(function () {
+  text = 'The cat in the hat.';
+  markovMachine = new MarkovMachine(text);
+  chains = markovMachine.getChains();
+});
+
+describe("test getChains", function () {
+
+
+  test('check if the returned chains are generated properly', function () {
+    expect(chains).toEqual({
+      'The': ['cat'],
+      'cat': ['in'],
+      'in': ['the'],
+      'the': ['hat.'],
+      'hat.': [null],
+    });
+  });
+
+
+});
+
+describe("test getText", function () {
+  test("test if the text generated is a string", function () {
+    expect(markovMachine.getText()).toEqual(expect.any(String));
+  });
+});
